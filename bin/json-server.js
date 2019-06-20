afccc3375eeac3e481fe1253edf09103552e39a6
Li Dong <lidongbest5@gmail.com>
2019-06-21 00:39:26 +0800

    update TEST

--- bin/json-server.js
+++ bin/json-server.js
@@ -5,9 +5,7 @@ const { toJsonServer } = require("@36node/query-normalizr");
 
 const mock = require("../mock");
 
-const { db = {}, rewrites = {}, routers = [] } = mock({
-  base: "/chart/v0",
-});
+const { db = {}, rewrites = {}, routers = [] } = mock();
 
 const server = jsonServer.create();
 const dbRouter = jsonServer.router(db);
@@ -28,6 +26,6 @@ server.use((req, res, next) => {
 routers.forEach(router => server.use(router));
 server.use(dbRouter);
 
-server.listen(3050, () => {
+server.listen(3000, () => {
   console.log("JSON Server is running");
 });
